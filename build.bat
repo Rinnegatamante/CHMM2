eb78d720619f28c2031604a3b7ce306c5b2a669d
Rinnegatamante <rinnegatamante@gmail.com>
2015-05-02 12:19:17 +0200

    Added builder script for 3DS/CIA formats.
    Added builder script for 3DS/CIA formats.

---
+++ build.bat
@@ -0,0 +1,3 @@
+arm-none-eabi-strip CHMM.elf
+makerom2 -f cci -o CHMM.3ds -rsf gw_workaround.rsf -target d -exefslogo -elf CHMM.elf -icon icon.bin -banner banner.bin
+makerom -f cia -o CHMM.cia -elf CHMM.elf -rsf cia_workaround.rsf -icon icon.bin -banner banner.bin -exefslogo -target t 
