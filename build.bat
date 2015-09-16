a832efacafcc3500fd946036d3d6332fa1780f52
Rinnegatamante <rinnegatamante@gmail.com>
2015-09-16 11:12:21 +0200

    Added assets compiling for CHMM2 new UI.
    Added assets compiling for CHMM2 new UI.

--- build.bat
+++ build.bat
@@ -1,4 +1,8 @@
 bin2c source/index.lua source/index.cpp index_lua
+bin2c data/icons.png source/icons.cpp icons_png
+bin2c data/icon.png source/icon.cpp icon_png
+bin2c data/voice.png source/voice.cpp voice_png
+bin2c data/main.ttf source/font.cpp font_ttf
 make
 arm-none-eabi-strip CHMM.elf
 makerom2 -f cci -o CHMM.3ds -rsf gw_workaround.rsf -target d -exefslogo -elf CHMM.elf -icon icon.bin -banner banner.bin
