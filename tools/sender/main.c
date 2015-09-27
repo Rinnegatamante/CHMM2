11da3bac13156792fca8c6bc1bdbb74e2b3fb1fc
Rinnegatamante <rinnegatamante@gmail.com>
2015-09-27 16:43:11 +0200

    Minor bugfixes.
    Minor bugfixes.

--- tools/sender/main.c
+++ tools/sender/main.c
@@ -17,6 +17,11 @@ typedef struct
 	struct sockaddr_in addrTo;
 } Socket;
 
+int setSockNoBlock(u32 s, u32 val)
+{
+	return setsockopt(s, SOL_SOCKET, 0x1009, (const char*)&val, sizeof(u32));
+}
+
 int main(int argc,char** argv){
 
 	// Getting IP
@@ -29,16 +34,18 @@ int main(int argc,char** argv){
 	
 	// Creating client socket
 	Socket* my_socket = (Socket*) malloc(sizeof(Socket));
+	memset(&my_socket->addrTo, '0', sizeof(my_socket->addrTo)); 
 	my_socket->addrTo.sin_family = AF_INET;
 	my_socket->addrTo.sin_port = htons(5000);
 	my_socket->addrTo.sin_addr.s_addr = inet_addr(host);
-	my_socket->sock = socket(AF_INET, SOCK_STREAM, 0);
 	my_socket->sock = socket(AF_INET, SOCK_STREAM, 0);
 	if (my_socket->sock < 0){
 		printf("\nFailed creating socket.");	
 		return -1;
 	}else printf("\nClient socket created on port 5000");
-	fcntl(my_socket->sock, F_SETFL, O_NONBLOCK);
+	
+	 // Set non-blocking 
+	setSockNoBlock(my_socket->sock, 1);
 	
 	// Connecting to CHMM2
 	int err = connect(my_socket->sock, (struct sockaddr*)&my_socket->addrTo, sizeof(my_socket->addrTo));
@@ -54,9 +61,10 @@ int main(int argc,char** argv){
 	while (count < 0){
 		int count = recv(my_socket->sock, &data, 11, 0);
 	}
-	if (strcmp(data,"YATA SENDER") == 0) printf("\nMagic received, starting transfer...");
+	printf(data);
+	if (strncmp(data,"YATA SENDER",11) == 0) printf("\nMagic received, starting transfer...");
 	else{
-		printf("Wrong magic received, connection aborted.");
+		printf("\nWrong magic received, connection aborted.");
 		close(my_socket->sock);
 		return -1;
 	}
@@ -69,8 +77,10 @@ int main(int argc,char** argv){
 		close(my_socket->sock);
 		return -1;
 	}
-	int size = fseek(input, 0, SEEK_END);
+	fseek(input, 0, SEEK_END);
+	int size = ftell(input);
 	fseek(input, 0, SEEK_SET);
+	printf("Done! (%i KBs)",(size/1024));
 	char* buffer = (char*)malloc(size);
 	fread(buffer, size, 1, input);
 	fclose(input);
